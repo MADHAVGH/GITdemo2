ee6c6d67e511ecf9451b0fdd8f5e6ddb57eec632
MADHAVGH <madhavreddy543@yahoo.in>
2020-06-24 16:22:42 +0530

    my commit for Gitstuff2 folder

---
+++ gitCheckArtifact/src/test/java/GitCheckGroup/gitCheckArtifact/gitTest.java
@@ -0,0 +1,15 @@
+package GitCheckGroup.gitCheckArtifact;
+
+public class gitTest {
+
+	public static void main(String[] args) {
+		System.out.println("Print GIT ALL");
+		System.out.println("Print GIT ALL");
+		System.out.println("Print GIT ALL");
+		System.out.println("Print GIT ALL");
+		System.out.println("Print GIT ALL");
+		System.out.println("Print GIT ALL");
+
+	}
+
+}
