667d81b1797d5cc3af45fd64a7d069fdb52efc7f
MADHAVGH <madhavreddy543@yahoo.in>
2020-06-24 19:04:13 +0530

    commit 3rd changes to GITdemo2

--- gitCheckArtifact/src/test/java/GitCheckGroup/gitCheckArtifact/gitTest.java
+++ gitCheckArtifact/src/test/java/GitCheckGroup/gitCheckArtifact/gitTest.java
@@ -9,6 +9,7 @@ public class gitTest {
 		System.out.println("Print GIT ALL");
 		System.out.println("Print GIT ALL** NEW NEW NEW");
 		System.out.println("Print GIT ALL** NEW NEW NEW");
+		System.out.println("add new peice of code");
 
 	}
 
